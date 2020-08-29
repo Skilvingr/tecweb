5333c476adc3ca2847c2c8cc80e8e82fed77b847
Alhazred98 <47506836+Alhazred98@users.noreply.github.com>
2020-08-29 16:44:57 +0200

    Update Create.js

--- create/Create.js
+++ create/Create.js
@@ -1,14 +1,18 @@
-var title = null;
+//variabili json
+var title ;
 var bottoni = null;
-var numPag=1;
+var numPag = 1;
 
 var controlBut = 0;
-var controlObj = 0;
-var controlHint = 0;
+var controlL = 0;
+
 
 var numButs;
 var numObj;
 var numHint;
+
+var modificatore;
+var arrayBut = ["Continue Button","Stop Button","Wrong button"];
 
 var Storia;
 
@@ -16,18 +20,31 @@ function logz() {
   console.log("faccio qualcosa di globale");
 }
 
+var obj = {
+
+};
+
 //Si occupa di creare la pagina
 function start() {
     //document.getElementById("titoloStory").value="ciao";
-    // title = document.getElementById("titoloStory").value;
+    //title = document.getElementById("titoloStory").value;
     //document.getElementById("myText").innerHTML = title;
-    
+
     var pageN = document.createElement("p");
-    var np = document.createTextNode("Paggina "+numPag);
+    var np = document.createTextNode("Pagina");
     pageN.appendChild(np);
 
-    var title = document.getElementById("titoloStory").value;
+
+    modificatore = document.createElement("input");
+    modificatore.setAttribute("id","numeropag");
+    modificatore.style.textAlign="center";
+    modificatore.size="1";
+    modificatore.value = numPag;
+    modificatore.readOnly=true;
+
+    title = document.getElementById("titoloStory").value;
     document.getElementById("titolo").innerHTML="";
+    obj.title = title;
 
     var page = document.getElementById("page");
 
@@ -35,7 +52,14 @@ function start() {
     var storyTitle = document.createTextNode(title);
     stor.appendChild(storyTitle);
 
-    var story = document.createElement("h2");
+    var storyMis = document.createElement("h2");
+    var titleMis = document.createTextNode("Inserisci titolo");
+    storyMis.appendChild(titleMis);
+
+    var titoloMis = document.createElement("input");
+    titoloMis.setAttribute("id","TitoloMissione");
+
+    var story = document.createElement("h3");
     var title = document.createTextNode("Inserisci storia");
     story.appendChild(title);
 
@@ -43,7 +67,7 @@ function start() {
     textarea.setAttribute("id", "textS");
     textarea.setAttribute("rows", "7");
     textarea.setAttribute("cols", "100");
-    
+
     var p = document.createElement("p");
     var p1 = document.createTextNode("Scegli il numero di bottoni");
     p.appendChild(p1);
@@ -65,53 +89,7 @@ function start() {
     var button = document.createElement("button");
     button.textContent = "Crea";
     button.setAttribute("onClick", "createButs()");
-    
-    var a = document.createElement("p");
-    var a1 = document.createTextNode("Scegli il numero di Oggetti");
-    a.appendChild(a1);
 
-  //Crea array per gli oggetti
-  var arrayObj = [0,1,2,3,4,5,6,7,8,9];
-
-  //Crea la lista
-  var selectListObj = document.createElement("select");
-  selectListObj.id = "listObj";
-
-  //Aggiunge le opzioni nella lista
-  for (var i = 0; i < arrayObj.length; i++) {
-    var optionObj = document.createElement("option");
-    optionObj.value = arrayObj[i];
-    optionObj.text = arrayObj[i];
-    selectListObj.appendChild(optionObj);
-  }
-
-    var object = document.createElement("button");
-    object.textContent = "Crea";
-    object.setAttribute("onClick", "createObj()");
-
-    var b = document.createElement("p");
-    var b1 = document.createTextNode("Scegli il numero di Indizi");
-    b.appendChild(b1);
-
-  //Crea array per gli Indizi
-  var arrayHint = [0,1,2,3,4,5,6,7,8,9];
-
-  //Crea la lista
-  var selectListHint = document.createElement("select");
-  selectListHint.id = "listHint";
-
-  //Aggiunge le opzioni nella lista
-  for (var i = 0; i < arrayHint.length; i++) {
-    var optionHint = document.createElement("option");
-    optionHint.value = arrayHint[i];
-    optionHint.text = arrayHint[i];
-    selectListHint.appendChild(optionHint);
-  }
-
-    var hint = document.createElement("button");
-    hint.textContent = "Crea";
-    hint.setAttribute("onClick", "createHint()");
-    
     var next = document.createElement("button");
     next.textContent = "Avanti";
     next.style.float="right";
@@ -120,28 +98,39 @@ function start() {
     var prev = document.createElement("button");
     prev.textContent = "Indietro";
     prev.style.float="left";
-    prev.setAttribute("onClick", "");
+    prev.setAttribute("onClick", "prev()");
+
+    var end = document.createElement("button");
+    end.textContent = "Fine";
+    end.style.float="center";
+    end.setAttribute("onClick", "end()");
+
+    var g = document.createElement("p");
+    var g1 = document.createTextNode("Grafico");
+    var x;
+    var i=0;
+
+    g.appendChild(g1);
 
     numPage.appendChild(pageN);
-    
+    numPage.appendChild(modificatore);
+
     page.appendChild(stor);
+    page.appendChild(storyMis);
+    page.appendChild(titoloMis);
     page.appendChild(story);
     page.appendChild(textarea);
     page.appendChild(p);
     page.appendChild(selectList);
     page.appendChild(button);
 
-    obj.appendChild(a);
-    obj.appendChild(selectListObj);
-    obj.appendChild(object);
-
-    hin.appendChild(b);
-    hin.appendChild(selectListHint);
-    hin.appendChild(hint);
-
     nextPrev.appendChild(next);
     nextPrev.appendChild(prev);
+
+    End.appendChild(end);
+    Graph.appendChild(g);
 }
+
 //Si occupa di creare i bottoni
 function createButs(){
 
@@ -151,126 +140,193 @@ function createButs(){
   //Controlla se son già creati dei bottoni
   if(controlBut!=0)
     document.getElementById("buts").innerHTML="";
-  
+
   for (var i = 0; i < elem; i++) {
 
     var input = document.createElement("input");
     input.setAttribute("id","button"+val);
-    
-       // creating checkbox element 
-       var checkbox = document.createElement('input'); 
-              
-       // Assigning the attributes 
-       // to created checkbox 
-       checkbox.type = "checkbox"; 
-       checkbox.name = "name"; 
-       checkbox.value = "value"; 
-       checkbox.id = "id"+val; 
-
-        // creating label for checkbox 
-        var label = document.createElement('label'); 
-              
-        // assigning attributes for the created label tag  
-        label.htmlFor = "id";
-        label.appendChild(document.createTextNode('Continue button'));  
 
     var p = document.createElement("p");
     var p1 = document.createTextNode(val+"° bottone");
     p.appendChild(p1);
 
-  
+    //Crea la lista
+    var selectListBut = document.createElement("select");
+    selectListBut.setAttribute("id","listBut"+val);
+      //Aggiunge le opzioni nella lista
+      for (var a = 0; a < arrayBut.length; a++) {
+        var optionBut = document.createElement("option");
+        optionBut.value = arrayBut[a];
+        optionBut.text = arrayBut[a];
+        selectListBut.appendChild(optionBut);
+      }
+
+    var div = document.createElement("div");
+    div.setAttribute("id","div"+val);
+    div.style.textAlign="center";
+    div.style.padding="10px";
+
     buts.appendChild(p);
     buts.appendChild(input);
-    buts.appendChild(checkbox);
-    buts.appendChild(label);
+    buts.appendChild(selectListBut);
+    buts.appendChild(div);
+
     val++;
   }
+
+  var button = document.createElement("button");
+  button.textContent = "Aggiungi Opzioni";
+  button.setAttribute("onClick","controlList()");
+
+  buts.appendChild(button);
+
   numButs = val-1;
   controlBut = 1;
-  
-} 
-
-function createObj(){
-
-  var objvar = 1;
-  var elemObj = document.getElementById("listObj").value;
-
-  //Controlla se non ci sono già bottoni creati
-  if(controlObj!=0)
-  document.getElementById("objButs").innerHTML="";
-
-  for (var i = 0; i < elemObj; i++) {
-
-  var object = document.createElement("input");
-    object.setAttribute("id","object"+objvar);
-
-    var p = document.createElement("p");
-    var p1 = document.createTextNode(objvar+"° oggetto");
-    p.appendChild(p1);
-
-    objvar++;
-
-    objButs.appendChild(p);
-    objButs.appendChild(object);
-  }
-  numObj = objvar-1;
-  controlObj = 1;
+  a = 0;
 }
 
+function controlList(){
 
-function createHint(){
+  var temp = 1;
+  var selector = numButs;
 
-  var hintvar = 1;
-  var elemHint = document.getElementById("listHint").value;
+  if(controlL!=0){
+    for(var a = 0; a < selector; a++){
+    document.getElementById("div"+temp).innerHTML="";
+    temp++;
+   }
+   temp = 1;
+  }
 
-    //Controlla se non ci sono già bottoni creati
-    if(controlHint!=0)
-    document.getElementById("hinButs").innerHTML="";
+  for(var i = 0; i < selector; i++){
 
-  
-  for (var i = 0; i < elemHint; i++) {
+  var listContent = document.getElementById("listBut"+ temp);
+  var div = document.getElementById("div"+ temp);
 
-  var hint = document.createElement("input");
-    hint.setAttribute("id","hint"+hintvar);
+  if(listContent.value == arrayBut[0]){
 
     var p = document.createElement("p");
-    var p1 = document.createTextNode(hintvar+"° Indizio");
+    var p1 = document.createTextNode("Inserire Destinazione");
     p.appendChild(p1);
 
-    hintvar++;
+    var input = document.createElement("input");
+    input.setAttribute("id","input"+ temp);
+      div.appendChild(p);
+      div.appendChild(input);
 
-    hinButs.appendChild(p);
-    hinButs.appendChild(hint);
+  }else if (listContent.value == arrayBut[1]){
+
+    var p = document.createElement("p");
+    var p1 = document.createTextNode("Inserire alert");
+    p.appendChild(p1);
+
+    var inputS = document.createElement("input");
+    inputS.setAttribute("id","stop"+temp);
+
+    div.appendChild(p);
+    div.appendChild(inputS);
   }
-   numHint = hintvar-1;
-   controlHint = 1;
+  temp++;
+}
+controlL = 1;
+
 }
 
 function next(){
-numPag++;
+  //azzera tutti gli input e cambia pagina
+  var controlTitle = document.getElementById("TitoloMissione");
 
-/*
-document.getElementById("textS").value="";
+  if(controlTitle.value=="")
+  alert("é necessario un titolo");
+  else{
 
-var pippo = document.getElementById("textS").value;
-var temp =1;
+  var temp = 1;
+  var temp2 = 1;
+  var page = "page"+numPag;
+      obj[page]={};
+      obj[page].missione = document.getElementById("TitoloMissione").value;
+      obj[page].testo = document.getElementById("textS").value;
 
-for(var i = 0; i < numButs ;i++){
-  
-  var pippo2 = document.getElementById("button"+temp).value;
+      for(var i = 0; i < numButs; i++){
+      var buttonval = document.getElementById("button"+temp).value;
+      var buttoninp = document.getElementById("listBut"+temp).value;
+      
 
-  var a = document.createElement("p");
-  var a1 = document.createTextNode(pippo2);
-  a.appendChild(a1);
-  temp++;
-  container.appendChild(a);
+      var propertyb = "bottone" + temp;
+      obj[page][propertyb] = buttonval;
+
+      var scelta = "scelta bottone"+temp;
+      
+      if(buttoninp == arrayBut[0]){
+      var listaBut = document.getElementById("input"+temp).value;
+      obj[page][scelta] = 0;
+      var destBut = "destinazione bottone"+temp;
+      obj[page][destBut] = listaBut;
+      }
+      else if(buttoninp == arrayBut[1]){
+      var listaBut = document.getElementById("stop"+temp).value;
+      obj[page][scelta] = 1;
+      var destBut = "alert bottone"+temp;
+      obj[page][destBut] = listaBut;
+      }
+      else
+      obj[page][scelta] = 2;
+      
+      
+        temp ++;
+      }
+
+        var title = controlTitle.value;
+        controlTitle.value="";
+        document.getElementById("textS").value="";
+        document.getElementById("numeropag").value= ++numPag;
+        document.getElementById("list").value=0;
+        document.getElementById("buts").innerHTML="";
+
+        var br = document.createElement("br");
+
+        var button = document.createElement("button");
+        button.setAttribute("id","buttonGraph"+ temp2);
+        button.textContent = title;
+      temp2++;
+    Graph.appendChild(br);
+    Graph.appendChild(button);
+
+  }
+
 }
 
-var p = document.createElement("p");
-    var p1 = document.createTextNode(pippo);
-    p.appendChild(p1);
 
-    container.appendChild(p);
-    //document.getElementById("container").innerHTML="";
-    */
+function prev(){
+  var temp=1;
+  document.getElementById("page").innerHTML="";
+  //var control = document.getElementById("buttonGraph"+temp).textContent;
+  //console.log(obj.missione);
+  //for(var x in obj){
+    //console.log(obj[x]);
+  //}
+  //console.log(obj);
+  //if(obj.missione == control){
+
+    //var titolo = document.createElement("h1");
+    //var h = document.createTextNode(obj.missione + obj.bottone[1]);
+    //titolo.appendChild(h);
+
+    //page.appendChild(titolo);
+
+  //}
+console.log(obj);
+
+}
+
+function end(){
+
+document.getElementById("body").innerHTML="";
+
+var titolo = document.createElement("h1");
+var h = document.createTextNode("Storia Creata");
+titolo.appendChild(h);
+
+body.appendChild(titolo);
+
 }
